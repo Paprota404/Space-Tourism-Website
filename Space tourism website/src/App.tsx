f93cb1bd049922ad7a70de2dcab471c5b9aa2e81
Paprota <paprota404@gmail.com>
2023-12-25 18:19:08 +0100

    Flying high in space

--- Space tourism website/src/App.tsx
+++ Space tourism website/src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Home from './Home';
+
 import Destination from './Destination';
+import backgroundImg from '/background-home-desktop.jpg'
 
 
 
@@ -14,8 +15,13 @@ function App() {
 
   return (
     <>
-    
-       <nav id="navbar">
+      <svg id="home-icon" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+      <circle cx="24" cy="24" r="24" fill="white"/>
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M24 0C24 0 24 24 0 24C23.5776 24.1714 24 48 24 48C24 48 24 24 48 24C24 24 24 0 24 0Z" fill="#0B0D17"/>
+      </svg>
+
+      <hr id="line"></hr>
+       <nav style={{zIndex:"2"}} id="navbar">
        {sections.map((section, index) => (
          <a 
            key={index} 
@@ -30,7 +36,7 @@ function App() {
       
       
      
-      {currentSection === '00 Home' && <Home />}
+      {currentSection === '00 Home' && <Home setCurrentSection={setCurrentSection}/>}
       {currentSection === '01 Destination' && <Destination />}
       {/*{currentSection === '02 Crew' && <Crew />}
        {currentSection === '03 Technology' && <Technology />} */}
@@ -40,3 +46,28 @@ function App() {
 }
 
 export default App
+
+function Home({setCurrentSection}){
+  return (
+      <div style={{backgroundImage:`url(${backgroundImg})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+      position:"absolute",
+      zIndex:"1",
+      height: '100vh',
+      width: '100%'}}>
+          
+          
+      <button id="explore" >EXPLORE</button>
+      <button id="hidden" onClick={() => setCurrentSection('01 Destination')}></button>
+      <div id="introduction">
+          <h5 >SO,YOU WANT TRAVEL TO SPACE</h5>
+          <h1 id="space">SPACE</h1>
+          <article id="intro">Let’s face it; if you want to go to space, you might as well genuinely go to 
+          outer space and not hover kind of on the edge of it. Well sit back, and relax 
+          because we’ll give you a truly out of this world experience!</article>
+      </div>
+      
+      </div>
+  )
+}
--- Space tourism website/src/Destination.tsx
+++ Space tourism website/src/Destination.tsx
@@ -1,29 +1,53 @@
-import backgroundImg from '/background-destination-desktop.jpg'
+import backgroundImg from '/background-destination-desktop.jpg';
+import Moon from '/image-moon.png';
+import { useState } from 'react';
+import Mars from '/image-mars.png';
+import Titan from '/image-titan.png';
+import Europa from '/image-europa.png'
 
 export default function Destination(){
+
+    const [planet,setPlanet] = useState({name:"MOON",desc:"See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",dist:"384,400 km",time:"3 days",link:"/image-moon.png"});
+
+function changePlanet(planeta){
+    setPlanet(planeta);
+}
+
     return (
-        <div style={{backgroundImage:`url(${backgroundImg})`,
+<div style={{backgroundImage:`url(${backgroundImg})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center center',
         height: '100vh',
+        position:"absolute",
+        zIndex:"1",
         width: '100%'}}>
+
             <h5 id="destination"><span style={{opacity:0.25}}>01&nbsp;</span> PICK YOUR DESTINATION</h5>
-            
-  Mars
-  Europa
-  Titan
+            <img id="planet" src={planet.link}></img>
 
-  Mars
+            <div id="planet-description">
+            <div id="planets">
+                <a id="planeta" onClick={()=>changePlanet({name:"MOON",desc:"See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",dist:"384,400 km",time:"3 days",link:"/image-moon.png"})} >MOON</a>
 
-  Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, 
-  the tallest planetary mountain in our solar system. It’s two and a half times 
-  the size of Everest!
+                <a id="planeta" onClick={()=>changePlanet({name:"MARS",desc:"Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountainin our solar system. It’s two and a half times the size of Everest!",dist:"225 mil. km", time:"9 months",link:"/image-mars.png"})} >MARS</a>
 
-  Avg. distance
-  225 mil. km
+                <a id="planeta" onClick={()=>changePlanet({name:"EUROPA",desc:"The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",dist:"628 mil. km", time:"3 years",link:"/image-europa.png"})}>EUROPA</a>
 
-  Est. travel time
-  9 months
+                <a id="planeta" onClick={()=>changePlanet({name:"TITAN",desc:"The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",dist:"1.6 bil. km", time:"7 years",link:"/image-titan.png"})}>TITAN</a>
+            </div>
+
+        <h2 id="planet-name">{planet.name}</h2>
+
+        <article>{planet.desc}</article>
+        <hr style={{opacity:"0.2",marginTop:"3rem",marginBottom:"2rem"}}></hr>
+
+        <div id="addons">
+        <span id="subheading2">Avg. distance</span>
+        <span id="subheading2"> Est. travel time</span>
+        <span id="subheading">{planet.dist}</span>
+        <span id="subheading">{planet.time}</span>
         </div>
+    </div>
+</div>
     )
 }
